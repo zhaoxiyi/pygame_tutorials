736fa6a6e49a0f813ab912ac74dbc07868cb68e6
zhaoxiyi <zhaoxiyi@hotmail.com>
2019-06-16 20:55:39 +0800

    input class2

--- sonprogram/firstgame.py
+++ sonprogram/firstgame.py
@@ -1,2 +1,15 @@
 import pygame
 import random
+
+#define screen size
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+
+pygame.quit()
